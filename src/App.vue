31ebb6e9b174c8bdc7f924621685ca302c3361d8
Victoria Cheng <victoria.cheng15@gmail.com>
2024-03-05 10:13:34 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { css } from "../styled-system/css"
+import { API_URL } from "./utils";
+
+console.log(API_URL)
+</script>
+
+<template>
+  <div :class="css({ fontSize: '5xl', fontWeight: 'bold', color: 'blue' })">Hello 🐼!</div>
+</template>
