ac6742072c179fc19e3d0dc7ba9d1463f1289ca7
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-03-06 10:37:22 -0700

    add components (#2)
    * chore: add section warapper
    
    * chore: add label text component
    
    * chore: add initial structure for currency component
    
    * chore: overall css strucutre and add currency component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import { css } from "../styled-system/css"
-import { API_URL } from "./utils";
-
-console.log(API_URL)
+import Currency from "./components/Currency.vue";
 </script>
 
 <template>
-  <div :class="css({ fontSize: '5xl', fontWeight: 'bold', color: 'blue' })">Hello 🐼!</div>
+  <div :class="css({ minH: '100vh', bg: 'gray.100' })">
+    <div :class="css({ mx: 'auto', maxW: '4xl', px: '4', pt: '8' })">
+      <header :class="css({ my: '10' })">
+        <h1 :class="css({ fontSize: '4xl', textAlign: 'center' })">Currency Converter</h1>
+      </header>
+      <main>
+        <Currency />
+      </main>
+    </div>
+  </div>
 </template>
---
+++ src/components/SectionWrapper.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { css } from "../../styled-system/css"
+
+const styles = css({ display: 'grid', gap: '2' })
+</script>
+
+<template>
+  <section
+    :class="css({ p: '4', display: 'flex', gap: '8', justifyContent: 'space-evenly', alignItems: 'flex-start', borderWidth: '2', borderRadius: 'md', borderColor: 'gray.400' })">
+    <div :class="styles">
+      <slot></slot>
+    </div>
+    <div :class="styles">
+      <slot name="secondLabel"></slot>
+    </div>
+  </section>
+</template>
