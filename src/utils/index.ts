6ecccc982dfbae5b5dced758e431de735cce27b5
Victoria Cheng <victoria.cheng15@gmail.com>
2024-03-06 10:34:34 -0700

    chore: add index imports exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,11 @@
-export const API_URL = import.meta.env.VITE_API_URL
+import { currencies } from "./currencies"
+
+const API_URL = import.meta.env.VITE_API_URL
+const API_KEY = import.meta.env.VITE_API_KEY
+
+const BASE_URL = `${API_URL}${API_KEY}`
+
+export {
+  BASE_URL,
+  currencies
+}
