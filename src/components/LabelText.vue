1381cb1ec3c47b854959020d6e83d7df5f93fe57
Victoria Cheng <victoria.cheng15@gmail.com>
2024-03-06 10:35:56 -0700

    chore: add label text component

---
+++ src/components/LabelText.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { css } from "../../styled-system/css"
+
+const { forElement, text } = defineProps<{ forElement: string, text: string }>()
+</script>
+
+<template>
+  <label :for="forElement" :class="css({ fontSize: 'xl', textAlign: 'center' })">
+    {{ text }}
+  </label>
+</template>
