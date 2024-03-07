d69158c43bff2998e2dfe2dbee1c1c2dbb697a5a
Victoria Cheng <victoria.cheng15@gmail.com>
2024-03-07 13:43:17 -0700

    chore: export api key and rmove api key from the base url

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,10 @@
 import { currencies } from "./currencies"
 
 const API_KEY = import.meta.env.VITE_API_KEY
-
-const BASE_URL = `https://api.freecurrencyapi.com/v1/latest?apikey=${API_KEY}`
+const BASE_URL = `https://api.freecurrencyapi.com/v1/latest`
 
 export {
   BASE_URL,
-  currencies
+  currencies,
+  API_KEY
 }
