0cb0d08c81c8abc13cd5e6bbfa52e1f0a966e5aa
Victoria Cheng <victoria.cheng15@gmail.com>
2024-03-07 15:33:46 -0700

    chore: revise css style

--- src/components/SectionWrapper.vue
+++ src/components/SectionWrapper.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { css } from "../../styled-system/css"
 
-const styles = css({ display: 'grid', gap: '2' })
+const styles = css({ display: 'grid', gap: '2', w: '40%' })
 </script>
 
 <template>
   <section
-    :class="css({ p: '4', display: 'flex', gap: '8', justifyContent: 'space-evenly', alignItems: 'flex-start', borderWidth: '2', borderRadius: 'md', borderColor: 'gray.400' })">
+    :class="css({ p: '4', display: 'flex', gap: '20', justifyContent: 'center', alignItems: 'flex-start', borderWidth: '2', borderRadius: 'md', borderColor: 'gray.400' })">
     <div :class="styles">
       <slot></slot>
     </div>
