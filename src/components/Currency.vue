973cb616cc08e838888acc31904f5d9589dddaf9
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-03-07 13:13:42 -0700

    add exchange rate (#5)
    * feat: add fetch exchange rate and display rates of the selected currencies
    
    * fix: add slash

--- src/components/Currency.vue
+++ src/components/Currency.vue
@@ -4,9 +4,11 @@ import { ref, computed, watch } from "vue"
 import SectionWrapper from "./SectionWrapper.vue";
 import LabelText from "./LabelText.vue"
 import { currencies as currencyList } from '../utils';
+import { BASE_URL } from "../utils";
 
 const base = ref("CAD")
 const currencies = ref<string[]>([])
+const exchangeRate = ref<Record<string, string>>({})
 
 const filteredList = computed(() => currencyList.filter(({ code }) => code !== base.value))
 
@@ -14,19 +16,35 @@ watch(base, (newBase) => {
   currencies.value = currencies.value.filter((code) => code !== newBase);
 });
 
+const getExchangeRate = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}&base_currency=${base.value}&currencies=${currencies.value}`)
+    const data = await response.json()
+    if (response.ok) {
+      exchangeRate.value = data.data
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function formattedRate(rate: string) {
+  return Number(rate).toFixed(2)
+}
 </script>
 
 <template>
   <SectionWrapper>
     <LabelText forElement="selectCurrency" text="The currency I have:" />
-    <select id="selectCurrency" v-model="base" :class="css({ p: '2', bg: 'gray.200' })">
+    <select @click="getExchangeRate" id="selectCurrency" v-model="base" :class="css({ p: '2', bg: 'gray.200' })">
       <option v-for="{ code, name } in currencyList" :value="code" :key="code">
         {{ name }} ({{ code }})
       </option>
     </select>
     <template v-slot:secondLabel>
       <LabelText forElement="selectCurrencies" text="The currencies I want:" />
-      <select id="selectCurrencies" v-model="currencies" :class="css({ p: '2', bg: 'gray.200', h: '200px' })" multiple>
+      <select @click="getExchangeRate" id="selectCurrencies" v-model="currencies"
+        :class="css({ p: '2', bg: 'gray.200', h: '200px' })" multiple>
         <option v-for="{ code, name } in filteredList" :value="code" :key="name" :class="css({ py: '1' })">
           {{ name }} ({{ code }})
         </option>
@@ -40,18 +58,22 @@ watch(base, (newBase) => {
       <span>{{ base }}</span>
       <span :class="css({ mx: '1' })">$</span>
       <input id="amountInput" type="number" placeholder="Enter amount"
-        :class="css({ p: '1', bg: 'red.100', borderRadius: 'lg' })" role="textbox" />
+        :class="css({ p: '1', bg: 'red.100', borderRadius: 'lg', textAlign: 'right' })" role="textbox" />
     </div>
 
     <template v-slot:secondLabel>
       <h2>Selected Currencies</h2>
       <ul :class="css({ display: 'grid', gap: '2' })">
         <li v-for="currency in currencies" :key="currency"
-          :class="css({ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', gap: '1' })">
-          <span :class="css({ flexBasis: '50px' })">{{ currency }}</span>
-          <span :class="css({ mx: '0' })">$</span>
-          <input type="number" value="1" :class="css({ p: '1', bg: 'red.100', borderRadius: 'lg', flexGrow: '1' })"
-            disabled />
+          :class="css({ display: 'flex', flexDir: 'column', gap: '1' })">
+          <div>
+            <span :class="css({ flexBasis: '50px' })">{{ currency }}</span>
+            <span :class="css({ mx: '1' })">$</span>
+            <input type="number" value="1"
+              :class="css({ p: '1', bg: 'red.100', borderRadius: 'lg', flexGrow: '1', textAlign: 'right' })" disabled />
+          </div>
+          <span :class="css({ textAlign: 'right', color: 'gray.500' })">Rate: {{ formattedRate(exchangeRate[currency])
+            }}</span>
         </li>
       </ul>
     </template>
