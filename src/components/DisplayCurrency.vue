4373c94dd641ccb0525c87f8fa08ffb9359eb08b
Victoria Cheng <victoria.cheng15@gmail.com>
2024-03-08 14:01:19 -0700

    chore: add display currency and dollar component

---
+++ src/components/DisplayCurrency.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { css } from "../../styled-system/css"
+
+const { displayCurrency } = defineProps<{ displayCurrency: string }>()
+</script>
+
+<template>
+  <span :class="css({ flexBasis: '30px' })">{{ displayCurrency }}</span>
+  <span :class="css({ mx: '1', fontSize: 'lg' })">$</span>
+</template>
