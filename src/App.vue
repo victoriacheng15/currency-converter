457b303fb8e867748dd2b05b046b73d5f5a13001
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-03-09 20:12:11 -0700

    add footer (#16)
    * chore: add footer
    
    * chore: include footer component and make footer stay at the bottom

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 import { css } from "../styled-system/css"
 import Currency from "./components/Currency.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-  <div :class="css({ minH: '100vh', bg: 'gray.100' })">
-    <div :class="css({ mx: 'auto', maxW: '4xl', px: '4', pt: '8' })">
+  <div :class="css({ bg: 'gray.100' })">
+    <div :class="css({ minH: '100vh', mx: 'auto', maxW: '4xl', px: '4', pt: '8', display: 'flex', flexDir: 'column' })">
       <header :class="css({ my: '10' })">
-        <h1 :class="css({ fontSize: '4xl', textAlign: 'center' })">Currency Converter</h1>
+        <h1 :class="css({ fontSize: '4xl', textAlign: 'center' })">Simple Plain Currency Converter</h1>
       </header>
-      <main>
+      <main :class="css({ mb: 'auto' })">
         <Currency />
       </main>
+      <Footer />
     </div>
   </div>
 </template>
